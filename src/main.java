b431e103f1ab2afdcc37dcddde057bd49d05f043
antoine <tnoo765300@gmail.com>
2015-10-16 15:52:23 +0200

    factorielle

---
+++ src/main.java
@@ -0,0 +1,23 @@
+
+public class main {
+/**
+ * 
+ * 
+ * 
+ * 
+ * @param args
+ */
+	public static void main(String[] args) {
+		
+		System.out.println("cou cou");
+		
+
+	}
+	
+	int factorielle() {
+		
+		
+		return 0;
+	}
+
+}
